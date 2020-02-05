0f6fbda0875ca4395b54249ff8bd724fcb668b36
Alexander Mandrykin <kfatti26@gmail.com>
2020-02-06 02:05:02 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 1.1.3. */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a; // переменная A
+        int n; // Степень числа A
+        double b;
+
+        Console.Write("Введите число A: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите степень числа A: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        b = Math.Pow(a, n);
+
+        Console.Write($"Полученное число = {b} \n");
+    }
+}
