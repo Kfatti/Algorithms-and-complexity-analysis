13427f67a221c94a53e25d2edd9bcaf91cb3f188
Alexander Mandrykin <kfatti26@gmail.com>
2020-02-06 02:03:00 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 1.1.2. */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a; // Основная переменная A
+        int b; // Основная переменная B
+
+        Console.Write("Введите число A: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите число B: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        a += b;
+        b = a - b;
+        a -=b;
+
+        Console.Write("Число A = " + a + "\n");
+        Console.Write("Число B = " + b + "\n");
+    }
+}
